c45ca3c26b4fc9fe55856127ee601e5e652689e6
mohamed youssef <mohamedyoussef@paymob.com>
2023-10-01 15:07:57 +0300

    no message

---
+++ LOGO/LOGOTests/Dummy/DummyEndpoint.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+@testable import LOGO
+
+final class DummyTarget: NetworkTarget {
+    var baseURL: LOGO.BaseURLType = .baseApi
+    
+    var version: LOGO.VersionType = .v1
+    
+    var path: String? = "test/test"
+    
+    var methodType: LOGO.HTTPMethod = .get
+    
+    var queryParams: [String : String]? = ["item": "item"]
+    
+    var queryParamsEncoding: LOGO.URLEncoding? = .default
+}
