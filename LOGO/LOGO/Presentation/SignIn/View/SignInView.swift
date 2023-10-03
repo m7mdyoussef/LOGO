2d07aa079aa2b5833808ac469899f9a113577bc6
mohamed youssef <mohamedyoussef@paymob.com>
2023-10-03 11:14:28 +0300

    fix signIn view

--- LOGO/LOGO/Presentation/SignIn/View/SignInView.swift
+++ LOGO/LOGO/Presentation/SignIn/View/SignInView.swift
@@ -22,65 +22,70 @@ struct SignInView: Coordinatable {
     //body
     var body: some View {
         NavigationView {
-            ZStack{
-                VStack{
-                    VStack(alignment: .center, spacing: 50.0){
-                        Image(Assets.loginPanner)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(minWidth: 375, maxHeight: 440, alignment: .center)
+            ScrollView(showsIndicators:false){
+                ZStack{
+                    VStack{
+                        VStack(alignment: .center, spacing: 30.0){
+                            Image(Assets.loginPanner)
+                                .resizable()
+                                .scaledToFill()
+//                                .frame(minWidth: UIScreen.screenWidth, maxHeight: UIScreen.screenHeight / 3, alignment: .center)
 
-                        Text(Constants.Auth.welcome)
-                            .foregroundColor(Color.primaryColor)
-                            .font(.title3)
-                            .fontWeight(.bold)
-                    }//: VStack
-                    Spacer()
-                    
-                    LazyVStack(spacing: 20){
-                                            
-                        AuthTextView(textValue: $viewModel.userName, isSecured: $viewModel.isSecured, isPassword:false, title: Constants.Auth.userNamePlaceHolder, authHeader: Constants.Auth.userNameTitle,keyboardType: .emailAddress)
-
-                        AuthTextView(textValue: $viewModel.password, isSecured: $viewModel.isSecured, isPassword:true, title: Constants.Auth.passwordPlaceHolder, authHeader: Constants.Auth.passwordTitle,keyboardType: .default)
-                                        
-                        Button {
-                            viewModel.signIn(user_Name: viewModel.userName, passwrd: viewModel.password)
+                            Text(Constants.Auth.welcome)
+                                .foregroundColor(Color.primaryColor)
+                                .font(.title3)
+                                .fontWeight(.bold)
                             
-                        }label: {
-                            Text (Constants.Auth.SignIn)
-                        }//: Button
-                        .disabled(!viewModel.enableSignIn)
-                        .padding(20)
-                        .font(.system(size: 17, design: .rounded))
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(Color .white)
-                        .background(viewModel.enableSignIn ? Color.primaryColor : Color.gray)
-                        .cornerRadius(.infinity)
-                    }//: LazyVStack
-                    .padding(.horizontal, 16)
-                    
-                    Spacer()
+                        }//: VStack
+                      //  Spacer()
+                        
+                        LazyVStack(spacing: 10){
+                                                
+                            AuthTextView(textValue: $viewModel.userName, isSecured: $viewModel.isSecured, isPassword:false, title: Constants.Auth.userNamePlaceHolder, authHeader: Constants.Auth.userNameTitle,keyboardType: .emailAddress)
 
-                }//: VStack
-                .ignoresSafeArea(.all, edges: [.top])
-                .onViewDidLoad {
-                    self.viewModel.apply(.onAppear)
-                }
-                
-                if !presentAlert{
-                    self.showAlert(Constants.Alert.Error, alertMesagee)
-                }
-                
-                if isLoading {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: Constant.cornerRadius)
-                            .foregroundColor(Color.white.opacity(0.9))
-                            .frame(width: 90.0, height: 90.0)
-                        ActivityIndicator(style: .large, animate: .constant(true))
+                            AuthTextView(textValue: $viewModel.password, isSecured: $viewModel.isSecured, isPassword:true, title: Constants.Auth.passwordPlaceHolder, authHeader: Constants.Auth.passwordTitle,keyboardType: .default)
+                              
+                            Spacer()
+                            Button {
+                                viewModel.signIn(user_Name: viewModel.userName, passwrd: viewModel.password)
+                                
+                            }label: {
+                                Text (Constants.Auth.SignIn)
+                            }//: Button
+                            .disabled(!viewModel.enableSignIn)
+                            .padding(20)
+                            .font(.system(size: 17, design: .rounded))
+                            .font(.title2)
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(Color .white)
+                            .background(viewModel.enableSignIn ? Color.primaryColor : Color.gray)
+                            .cornerRadius(.infinity)
+                        }//: LazyVStack
+                        .padding(.horizontal, 16)
+                        
+                        Spacer()
+
+                    }//: VStack
+                    .onViewDidLoad {
+                        self.viewModel.apply(.onAppear)
                     }
-                }
-            }//: ZStack
+                    
+                    if !presentAlert{
+                        self.showAlert(Constants.Alert.Error, alertMesagee)
+                    }
+                    
+                    if isLoading {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: Constant.cornerRadius)
+                                .foregroundColor(Color.white.opacity(0.9))
+                                .frame(width: 90.0, height: 90.0)
+                            ActivityIndicator(style: .large, animate: .constant(true))
+                        }
+                    }
+                }//: ZStack
+            }//: ScrollView
+            .ignoresSafeArea(.all, edges: [.top])
+            
         }//: NavigationView
         .onAppear(perform: handleState)
     }
